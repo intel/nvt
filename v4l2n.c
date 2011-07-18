01958fde6d7676064067a3ed94731aff8120b786
Tuukka Toivonen <tuukka.toivonen@intel.com>
2011-07-18 16:48:51 +0300

    v4l2n: add options --idsensor and --idflash

--- v4l2n.c
+++ v4l2n.c
@@ -8,19 +8,33 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <linux/videodev2.h>
+
+#include "linux/atomisp.h"
 
 char *name = "v4l2n";
 
+#define FALSE		0
+#define TRUE		(!FALSE)
+
+#define STRINGIFY_(x)	#x
+#define STRINGIFY(x)	STRINGIFY_(x)
+
+typedef unsigned char bool;
+
 struct {
 	char *device;
+	bool idsensor;
+	bool idflash;
 } args = {
 	.device = "/dev/video0",
 };
 
 struct {
-	int device;
+	int fd;
 } vars = {
-	.device = -1,
+	.fd = -1,
 };
 
 static void error(char *msg, ...)
@@ -39,11 +53,22 @@ static void error(char *msg, ...)
 	exit(1);
 }
 
+#define xioctl(io, arg) xioctl_(#io, io, arg)
+
+static void xioctl_(char *ios, int ion, void *arg)
+{
+	int r = ioctl(vars.fd, ion, arg);
+	if (r)
+		error("%s failed", ios);
+}
+
 static void usage(void)
 {
-	printf("Usage: %s [-h] [-d device]\n", name);
+	printf("Usage: %s [-h] [-d device] [--idsensor] [--idflash]\n", name);
 	printf(	"-h	Show this help\n"
-		"-d	/dev/videoX device node\n");
+		"-d	/dev/videoX device node\n"
+		"--idsensor	Get sensor identification\n"
+		"--idflash	Get flash identification\n");
 }
 
 static void get_options(int argc, char *argv[])
@@ -52,6 +77,8 @@ static void get_options(int argc, char *argv[])
 		static struct option long_options[] = {
 			{ "help", 0, NULL, 'h' },
 			{ "device", 1, NULL, 'd' },
+			{ "idsensor", 0, NULL, 1001 },
+			{ "idflash", 0, NULL, 1002 },
 			{ 0, 0, 0, 0 }
 		};
 
@@ -68,6 +95,14 @@ static void get_options(int argc, char *argv[])
 			args.device = optarg;
 			break;
 
+		case 1001:
+			args.idsensor = TRUE;
+			break;
+
+		case 1002:
+			args.idflash = TRUE;
+			break;
+
 		default:
 			error("unknown option");
 		}
@@ -76,15 +111,15 @@ static void get_options(int argc, char *argv[])
 
 static void open_device()
 {
-	vars.device = open(args.device, 0);
-	if (vars.device == -1)
+	vars.fd = open(args.device, 0);
+	if (vars.fd == -1)
 		error("failed to open %s", args.device);
 }
 
 static void close_device()
 {
-	close(vars.device);
-	vars.device = -1;
+	close(vars.fd);
+	vars.fd = -1;
 }
 
 int main(int argc, char *argv[])
@@ -93,6 +128,19 @@ int main(int argc, char *argv[])
 	name = argv[0];
 	get_options(argc, argv);
 	open_device();
+
+	if (args.idsensor) {
+		struct atomisp_model_id id;
+		xioctl(ATOMISP_IOC_G_SENSOR_MODEL_ID, &id);
+		printf("ATOMISP_IOC_G_SENSOR_MODEL_ID: [%s]\n", id.model);
+	}
+
+	if (args.idflash) {
+		struct atomisp_model_id id;
+		xioctl(ATOMISP_IOC_G_FLASH_MODEL_ID, &id);
+		printf("ATOMISP_IOC_G_FLASH_MODEL_ID: [%s]\n", id.model);
+	}
+
 	close_device();
 	return 0;
 }
