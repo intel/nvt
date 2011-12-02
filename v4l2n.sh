e020dc9a220dcacaba520673824e73b84e31a576
Tuukka Toivonen <tuukka.toivonen@intel.com>
2011-12-02 16:13:04 +0200

    v4l2n.sh: support for configurable parameters with environment variables
    Add support for configurable parameters into AUTO mode.
    The parameters are set as an environment variables.

--- v4l2n.sh
+++ v4l2n.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Some default settings. Override with environment variables
+if [ "$V4L2N_WIDTH"  = "" ]; then V4L2N_WIDTH=800;     fi
+if [ "$V4L2N_HEIGHT" = "" ]; then V4L2N_HEIGHT=600;    fi
+if [ "$V4L2N_FORMAT" = "" ]; then V4L2N_FORMAT=NV12;   fi
+if [ "$V4L2N_INPUT"  = "" ]; then V4L2N_INPUT=1;       fi
+if [ "$V4L2N_VIEWER" = "" ]; then V4L2N_VIEWER=gqview; fi
+
 P="$@"
 AUTO=""
 
@@ -11,17 +18,18 @@ if [ "$P" = "-a" ]; then
 	echo "Enabling AUTO mode"
 	AUTO=1
 	rm -f /tmp/testimage_000.raw /tmp/testimage_000.pnm
-	P="--input 1 --parm type=1 --fmt type=1,width=800,height=600,pixelformat=NV12 --reqbufs count=1,memory=USERPTR --capture=1 -o /sdcard/testimage_@.raw"
+	P="--input $V4L2N_INPUT --parm type=1 --fmt type=1,width=$V4L2N_WIDTH,height=$V4L2N_HEIGHT,pixelformat=$V4L2N_FORMAT --reqbufs count=1,memory=USERPTR --capture=1 -o /sdcard/testimage_@.raw"
 fi
 
 adb shell 'mount -o remount -w /dev/block/mmcblk0p6 /system' && \
 adb push v4l2n /system/ && \
+echo "/system/v4l2n $P" && \
 adb shell "/system/v4l2n $P"
 
 if [ "$AUTO" = "1" ]; then
 	adb pull /sdcard/testimage_000.raw /tmp/
 	if [ -s /tmp/testimage_000.raw ]; then
-		./raw2pnm -x 800 -y 600 -f NV12 /tmp/testimage_000.raw /tmp/testimage_000.pnm && \
-		gqview /tmp/testimage_000.pnm
+		./raw2pnm -x $V4L2N_WIDTH -y $V4L2N_HEIGHT -f $V4L2N_FORMAT /tmp/testimage_000.raw /tmp/testimage_000.pnm && \
+		$V4L2N_VIEWER /tmp/testimage_000.pnm
 	fi
 fi
