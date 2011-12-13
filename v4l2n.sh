6a1b8dc21df7f42b360218e2310a97783584cfea
Tuukka Toivonen <tuukka.toivonen@intel.com>
2011-12-13 21:06:44 +0200

    v4l2n.sh: change image capture directory to /cache

--- v4l2n.sh
+++ v4l2n.sh
@@ -9,6 +9,7 @@ if [ "$V4L2N_VIEWER" = "" ]; then V4L2N_VIEWER=gqview; fi
 
 P="$@"
 AUTO=""
+DIR=/cache
 
 if [ "$P" = "" ]; then
 	P="-h"
@@ -18,16 +19,23 @@ if [ "$P" = "-a" ]; then
 	echo "Enabling AUTO mode"
 	AUTO=1
 	rm -f /tmp/testimage_000.raw /tmp/testimage_000.pnm
-	P="--input $V4L2N_INPUT --parm type=1 --fmt type=1,width=$V4L2N_WIDTH,height=$V4L2N_HEIGHT,pixelformat=$V4L2N_FORMAT --reqbufs count=1,memory=USERPTR --capture=1 -o /sdcard/testimage_@.raw"
+	P=""
+	P="$P --input $V4L2N_INPUT"
+	P="$P --parm type=1"
+	P="$P --fmt type=1,width=$V4L2N_WIDTH,height=$V4L2N_HEIGHT,pixelformat=$V4L2N_FORMAT"
+	P="$P --reqbufs count=1,memory=USERPTR"
+	P="$P --capture=1"
+	P="$P -o $DIR/testimage_@.raw"
 fi
 
-adb shell 'mount -o remount -w /dev/block/mmcblk0p6 /system' && \
+adb shell 'mount -o remount -w /dev/block/mmcblk0p6 /system'
+adb shell rm -f "$DIR/testimage_*.raw"
 adb push v4l2n /system/ && \
 echo "/system/v4l2n $P" && \
 adb shell "/system/v4l2n $P"
 
 if [ "$AUTO" = "1" ]; then
-	adb pull /sdcard/testimage_000.raw /tmp/
+	adb pull $DIR/testimage_000.raw /tmp/
 	if [ -s /tmp/testimage_000.raw ]; then
 		./raw2pnm -x $V4L2N_WIDTH -y $V4L2N_HEIGHT -f $V4L2N_FORMAT /tmp/testimage_000.raw /tmp/testimage_000.pnm && \
 		$V4L2N_VIEWER /tmp/testimage_000.pnm
