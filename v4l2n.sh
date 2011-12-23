b31bb70f11e989d490b2a8a1a1736ac047fe3ddb
Tuukka Toivonen <tuukka.toivonen@intel.com>
2011-12-23 11:11:06 +0200

    v4l2n.sh: allow specifying device directory and capture two images
    Two improvements are made:
    - Allow specifying directory on device into which to store images
    - Capture two images

--- v4l2n.sh
+++ v4l2n.sh
@@ -6,10 +6,10 @@ if [ "$V4L2N_HEIGHT" = "" ]; then V4L2N_HEIGHT=600;    fi
 if [ "$V4L2N_FORMAT" = "" ]; then V4L2N_FORMAT=NV12;   fi
 if [ "$V4L2N_INPUT"  = "" ]; then V4L2N_INPUT=1;       fi
 if [ "$V4L2N_VIEWER" = "" ]; then V4L2N_VIEWER=gqview; fi
+if [ "$V4L2N_DIR"    = "" ]; then V4L2N_DIR=/cache;    fi
 
 P="$@"
 AUTO=""
-DIR=/cache
 
 if [ "$P" = "" ]; then
 	P="-h"
@@ -18,27 +18,35 @@ fi
 if [ "$P" = "-a" ]; then
 	echo "Enabling AUTO mode"
 	AUTO=1
-	rm -f /tmp/testimage_000.raw /tmp/testimage_000.pnm
+	rm -f /tmp/testimage_*.raw /tmp/testimage_*.pnm
 	P=""
 	P="$P --input $V4L2N_INPUT"
 	P="$P --parm type=1"
 	P="$P --fmt type=1,width=$V4L2N_WIDTH,height=$V4L2N_HEIGHT,pixelformat=$V4L2N_FORMAT"
-	P="$P --reqbufs count=1,memory=USERPTR"
-	P="$P --capture=1"
-	P="$P -o $DIR/testimage_@.raw"
+	P="$P --reqbufs count=2,memory=USERPTR"
+	P="$P --capture=2"
+	P="$P -o $V4L2N_DIR/testimage_@.raw"
 fi
 
 adb shell 'mount -o remount -w /dev/block/mmcblk0p6 /system'
-adb shell rm "$DIR/testimage_*.raw"
+adb shell rm "$V4L2N_DIR/testimage_*.raw"
 adb push v4l2n /system/ && \
 adb shell "/system/v4l2n --enuminput" && \
 echo "/system/v4l2n $P" && \
 adb shell "/system/v4l2n $P"
 
 if [ "$AUTO" = "1" ]; then
-	adb pull $DIR/testimage_000.raw /tmp/
-	if [ -s /tmp/testimage_000.raw ]; then
-		./raw2pnm -x $V4L2N_WIDTH -y $V4L2N_HEIGHT -f $V4L2N_FORMAT /tmp/testimage_000.raw /tmp/testimage_000.pnm && \
-		$V4L2N_VIEWER /tmp/testimage_000.pnm
+	for i in 0 1; do
+		adb pull $V4L2N_DIR/testimage_00$i.raw /tmp/
+		if [ -s /tmp/testimage_00$i.raw ]; then
+			./raw2pnm -x $V4L2N_WIDTH -y $V4L2N_HEIGHT -f $V4L2N_FORMAT /tmp/testimage_00$i.raw /tmp/testimage_00$i.pnm
+		fi
+	done
+	if [ -s /tmp/testimage_001.pnm ]; then
+		$V4L2N_VIEWER /tmp/testimage_001.pnm
+	else
+		if [ -s /tmp/testimage_000.pnm ]; then
+			$V4L2N_VIEWER /tmp/testimage_000.pnm
+		fi
 	fi
 fi
