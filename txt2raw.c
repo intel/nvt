ef744877af4f9110a3a60dd495f6e0f86b8d3867
Tuukka Toivonen <tuukka.toivonen@intel.com>
2013-11-12 16:56:49 +0200

    txt2raw: add function to expand 10-bits per pixel to 16-bits per pixel

--- txt2raw.c
+++ txt2raw.c
@@ -93,19 +93,68 @@ static unsigned char *txt2buf(FILE *f, int size[2])
 	return buf;
 }
 
+static unsigned char *uncompress(unsigned char *compr, int compr_size[2], int uncomp_size[2])
+{
+	unsigned char *buf;		/* Resulting buffer */
+	unsigned char *sa;		/* Source byte address */
+	unsigned char *da;		/* Destination byte address */
+	int y, x, b;			/* y, x, and b counters */
+	const int d_byp = 2;		/* Destination bytes per pixel */
+	const int s_bpp = 10;		/* Source bits per pixel */
+
+	uncomp_size[0] = compr_size[0] * 8 / s_bpp;	/* Size in pixels */
+	uncomp_size[1] = compr_size[1];
+	buf = calloc(uncomp_size[0] * d_byp * compr_size[1], 1);
+
+	for (y = 0; y < uncomp_size[1]; y++) {
+		unsigned int s;			/* Source byte */
+		int s_bits = 0;			/* Bits left in source byte */
+		sa = compr + compr_size[0] * y;
+		da = buf + uncomp_size[0] * d_byp * y;
+		for (x = 0; x < uncomp_size[0]; x++) {
+			unsigned int d = 0;	/* Destination pixel */
+			for (b = 0; b < s_bpp; b++) {
+				if (s_bits <= 0) {
+					s = *sa++;
+					s_bits = 8;
+				}
+				d <<= 1;
+				d |= s & 1;
+				s >>= 1;
+				s_bits--;
+
+			}
+			da[0] = d & 0xff;
+			da[1] = d >> 8;
+			da += d_byp;
+		}
+	}
+
+	return buf;
+}
+
 int main(int argc, char *argv[])
 {
 	char *name;
-	unsigned char *buf;
-	int size[2];
+	unsigned char *compr;
+	int compr_size[2];
+	unsigned char *uncomp;
+	int uncomp_size[2];
 
 	if (argc != 2) error("give exactly one argument for output filename\n");
 	name = argv[1];
 
-	buf = txt2buf(stdin, size);
+	compr = txt2buf(stdin, compr_size);
+	printf("read %ix%i bytes of data\n", compr_size[0], compr_size[1]);
 
-	printf("read %ix%i bytes of data, writing to %s\n", size[0], size[1], name);
-	write_file(name, buf, size[0] * size[1]);
+	uncomp = uncompress(compr, compr_size, uncomp_size);
+	printf("uncompressed to %ix%i pixels of data\n", uncomp_size[0], uncomp_size[1]);
+
+	printf("writing to %s\n", name);
+	write_file(name, uncomp, uncomp_size[0] * uncomp_size[1] * 2);
+
+	free(compr);
+	free(uncomp);
 
 	return 0;
 }
